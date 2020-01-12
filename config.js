a1c0a053b7792175ed168b2b6f63ef97ce3e133f
Immortal Evil <52874725+ImmortalEvil@users.noreply.github.com>
2020-01-12 16:48:18 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "You can get one at https://discordapp.com/developers",
+    prefix: 'YourPrefix',
+    c: "#ff0000",
+    coc_token: "You can get one at https://developer.clashofclans.com/"
+  };
